8ae3478ffd9119ebc3dc5ce3b99d23dedd4b9292
Thomas Luechtefeld <tom@insilica.co>
2025-05-31 20:16:13 -0400

    Add CSRF protection and token to login form

--- web/__init__.py
+++ web/__init__.py
@@ -1,12 +1,17 @@
 import os
 from flask import Flask
+from flask_wtf import CSRFProtect
 from .auth import bp as auth_bp, login_manager
 from .dashboard import bp as dashboard_bp
+
+csrf = CSRFProtect()
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev')
+
+    csrf.init_app(app)
 
     login_manager.init_app(app)
     app.register_blueprint(auth_bp)
